6893e00b950462bac42fd268cc8f4a8aac3889cc
Jason Gallavin <jasongallavin@gmail.com>
2019-07-15 22:56:57 -0400

    Add cors for convenience.

--- src/main/java/org/ius/csg/cslabs/esxi/WebServer.java
+++ src/main/java/org/ius/csg/cslabs/esxi/WebServer.java
@@ -42,6 +42,28 @@ public class WebServer
       simpleClient.connect();
       System.out.println("Webserver starting on http://localhost:4567");
 
+      options("/*",
+              (request, response) -> {
+
+                  String accessControlRequestHeaders = request
+                          .headers("Access-Control-Request-Headers");
+                  if (accessControlRequestHeaders != null) {
+                      response.header("Access-Control-Allow-Headers",
+                              accessControlRequestHeaders);
+                  }
+
+                  String accessControlRequestMethod = request
+                          .headers("Access-Control-Request-Method");
+                  if (accessControlRequestMethod != null) {
+                      response.header("Access-Control-Allow-Methods",
+                              accessControlRequestMethod);
+                  }
+
+                  return "OK";
+              });
+
+      before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
       get("/acquireTicket", (req, res) -> {
           res.type("application/json");
           if(!req.queryParams().contains("name")) {
